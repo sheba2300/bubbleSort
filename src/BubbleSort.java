2a97c4545b793c90aab0dc144e0d37b4af5b02d1
sheba sow <ssow8171@bths.edu>
2018-11-14 10:17:01 -0500

    making bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -7,6 +7,24 @@ public class BubbleSort {
         {
             int placeHolder = array[posOne];
             array[posOne] =array[posTwo];
+            array[posTwo] = placeHolder;
+        }
+        public void bubbleSort(int[] arr)
+        {
+            int range = arr.length;
+            boolean noSwap = false;
+            int noSwapIdx = 0;
+            while(range >= 0)
+            {
+                for(int x = 0; x<range; x++)
+                {
+                    if(arr[x]>arr[x+1])
+                    {
+                        swap(int[] arr, x, x+1)
+                        noSwap = false;
+                    }
+                }
+            }
         }
     }
 }
